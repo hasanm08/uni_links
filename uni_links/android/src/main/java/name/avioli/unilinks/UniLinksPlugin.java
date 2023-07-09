9d1d6a01b9a81cb3041a3a8b7e83e892201a4a2c
Amirhassan Amirmahani <39486543+hasanm08@users.noreply.github.com>
2023-07-09 11:53:24 +0330

    Update UniLinksPlugin.java-bard

--- uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -15,99 +15,98 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
 
-public class UniLinksPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler, EventChannel.StreamHandler, ActivityAware, PluginRegistry.NewIntentListener {
-    private static final String MESSAGES_CHANNEL = "uni_links/messages";
-    private static final String EVENTS_CHANNEL = "uni_links/events";
+public class UniLinksPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler, EventChannel.StreamHandler, ActivityAware {
+private static final String MESSAGES_CHANNEL = "uni_links/messages";
+private static final String EVENTS_CHANNEL = "uni_links/events";
+private BroadcastReceiver changeReceiver;
 
-    private BroadcastReceiver changeReceiver;
+private String initialLink;
+private String latestLink;
+private Context context;
 
-    private String initialLink;
-    private String latestLink;
-    private Context context;
+private void handleNewIntent(Intent intent) {
+    String action = intent.getAction();
+    String dataString = intent.getDataString();
 
-    private void handleIntent(Context context, Intent intent) {
-        String action = intent.getAction();
-        String dataString = intent.getDataString();
-
-        if (Intent.ACTION_VIEW.equals(action)) {
-            if (initialLink == null) {
-                initialLink = dataString;
-            }
-            latestLink = dataString;
-            if (changeReceiver != null) changeReceiver.onReceive(context, intent);
+    if (Intent.ACTION_VIEW.equals(action)) {
+        if (initialLink == null) {
+            initialLink = dataString;
         }
+        latestLink = dataString;
+        if (changeReceiver != null) changeReceiver.onReceive(context, intent);
     }
+}
 
-    @NonNull
-    private BroadcastReceiver createChangeReceiver(final EventChannel.EventSink events) {
-        return new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
+@NonNull
+private BroadcastReceiver createChangeReceiver(final EventChannel.EventSink events) {
+    return new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
 
-                // Log.v("uni_links", String.format("received action: %s", intent.getAction()));
+            // Log.v("uni_links", String.format("received action: %s", intent.getAction()));
 
-                String dataString = intent.getDataString();
+            String dataString = intent.getDataString();
 
-                if (dataString == null) {
-                    events.error("UNAVAILABLE", "Link unavailable", null);
-                } else {
-                    events.success(dataString);
-                }
+            if (dataString == null) {
+                events.error("UNAVAILABLE", "Link unavailable", null);
+            } else {
+                events.success(dataString);
             }
-        };
-    }
-
-    @Override
-    public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding flutterPluginBinding) {
-        this.context = flutterPluginBinding.getApplicationContext();
-        register(flutterPluginBinding.getBinaryMessenger());
-    }
-
-    private void register(BinaryMessenger messenger) {
-        final MethodChannel methodChannel = new MethodChannel(messenger, MESSAGES_CHANNEL);
-        methodChannel.setMethodCallHandler(this);
-
-        final EventChannel eventChannel = new EventChannel(messenger, EVENTS_CHANNEL);
-        eventChannel.setStreamHandler(this);
-    }
-
-    /** Plugin registration. */
-    public static void registerWith(PluginRegistry.Registrar registrar) {
-        // Detect if we've been launched in background
-        if (registrar.activity() == null) {
-            return;
         }
+    };
+}
 
-        final UniLinksPlugin instance = new UniLinksPlugin();
-        instance.context = registrar.context();
-        instance.register(registrar.messenger());
+@Override
+public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    this.context = flutterPluginBinding.getApplicationContext();
+    register(flutterPluginBinding.getBinaryMessenger());
+}
 
-        instance.handleIntent(registrar.context(), registrar.activity().getIntent());
-        registrar.addNewIntentListener(instance);
+private void register(BinaryMessenger messenger) {
+    final MethodChannel methodChannel = new MethodChannel(messenger, MESSAGES_CHANNEL);
+    methodChannel.setMethodCallHandler(this);
+
+    final EventChannel eventChannel = new EventChannel(messenger, EVENTS_CHANNEL);
+    eventChannel.setStreamHandler(this);
+}
+
+/** Plugin registration. */
+public static void registerWith(PluginRegistry.Registrar registrar) {
+    // Detect if we've been launched in background
+    if (registrar.activity() == null) {
+        return;
     }
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {}
+    final UniLinksPlugin instance = new UniLinksPlugin();
+    instance.context = registrar.context();
+    instance.register(registrar.messenger());
 
-    @Override
-    public void onListen(Object arguments, EventChannel.EventSink events) {
-        changeReceiver = createChangeReceiver(events);
-    }
+    instance.handleNewIntent(registrar.activity().getIntent());
+    registrar.addOnNewIntentListener(instance);
+}
 
-    @Override
-    public void onCancel(Object arguments) {
-        changeReceiver = null;
-    }
+@Override
+public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
 
-    @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-        if (call.method.equals("getInitialLink")) {
-            result.success(initialLink);
-        } else if (call.method.equals("getLatestLink")) {
-            result.success(latestLink);
-        } else {
-            result.notImplemented();
+@Override
+public void onListen(Object arguments, EventChannel.EventSink events) {
+    changeReceiver = createChangeReceiver(events);
+}
+
+@Override
+public void onCancel(Object arguments) {
+    changeReceiver = null;
+}
+
+@Override
+public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+    if (call.method.equals("getInitialLink")) {
+        result.success(initialLink);
+    } else if (call.method.equals("getLatestLink")) {
+        result.success(latestLink);
+    } else {
+        result.notImplemented();
         }
     }
 
